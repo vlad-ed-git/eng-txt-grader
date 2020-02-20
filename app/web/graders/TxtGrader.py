7ea73f060c2bcd4ac9954354b931de5b7ae316b7
Vladimir Peter <vlad.edna@gmail.com>
2020-02-20 13:43:46 +0800

    cleaned repo1

--- app/web/graders/TxtGrader.py
+++
@@ -1,7 +0,0 @@
-
-
-def extract_words_from_txt_file(input_txt_file_path):
-    input_txt_file = open(input_txt_file_path, "r")
-    words_in_txt = [word for line in input_txt_file for word in line.split()]
-    input_txt_file.close()
-    return words_in_txt
